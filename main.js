ed5dc92badf7d8bbe18f6d5bcc0a635e3781ebb5
requin883 <requin883@gmail.com>
2022-05-31 21:36:29 -0400

    feat: Create playGame function

--- main.js
+++ main.js
@@ -5,7 +5,30 @@ function computerPlay(){
 
 function userPlay(){
     let userMove = prompt("Choose rock,paper or scissors")
-    return userMove.toLowerCase();
+    return userMove[0].toUpperCase() + userMove.slice(1).toLowerCase();
 }
+function playGame(){
+    let resetGame = 0;
+    while (resetGame == 0){
+        let computerMove = computerPlay();
+        let userMove = userPlay();
+        let gameWinner = `Player played ${userMove} and Computer played ${computerMove} and `;
+        switch(true){
+            case (computerMove == userMove):
+                gameWinner += "This is a Draw!";
+                break;
+            case ((computerMove == "Rock" && userMove == "Paper") || 
+            (computerMove == "Scissors" && userMove == "Rock") ||
+            (computerMove == "Paper" && userMove == "Scissors")):
+                gameWinner += "Player wins!";
+                break;
+            default:
+                gameWinner += "Computer Wins!"
+                break;
+        }
+        console.log(gameWinner);
+        resetGame = parseInt(prompt("If you want stop playing please press any number"));
 
-console.log(userPlay());
+    }
+}
+playGame();
