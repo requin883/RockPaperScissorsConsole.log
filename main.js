c70cf340e6c32301fbea4c5a5fce2730b9f2f77e
requin883 <requin883@gmail.com>
2022-07-15 09:30:18 -0400

    feat: change functionality to show in screen

--- main.js
+++ main.js
@@ -1,44 +1,45 @@
+//DOM Items
+const container = document.querySelector(".btn-group")
+const btns = document.querySelectorAll("button");
+const winnerEl = document.createElement('h3');
+container.appendChild(winnerEl);
+
+btns.forEach(button=>button.addEventListener('click',(e)=> playGame(e.target["id"])));
+
+
 function computerPlay(){
         let computerMove = Math.floor(Math.random()*3)+1;
-        return computerMove == 1 ? "Rock" : computerMove == 2 ? "Paper" : "Scissors";
+        return computerMove == 1 ? "rock" : computerMove == 2 ? "paper" : "scissors";
 }
-
-function userPlay(){
-    let userMove = prompt("Choose rock,paper or scissors")
-    return userMove[0].toUpperCase() + userMove.slice(1).toLowerCase();
-}
-function playGame(){
+function playGame(userMove){
     let ComputerCounter = 0;
     let playerCounter = 0;
     let totalGames = 0;
         let computerMove = computerPlay();
-        let userMove = userPlay();
         let gameWinner = `Player played ${userMove} and Computer played ${computerMove} and `;
         switch(true){
             case (computerMove == userMove):
-                console.log(gameWinner+="This is a draw!")
+                winnerEl.textContent=  "This is a Draw!";
                 totalGames++;
                 break;
-            case ((computerMove == "Rock" && userMove == "Paper") || 
-            (computerMove == "Scissors" && userMove == "Rock") ||
-            (computerMove == "Paper" && userMove == "Scissors")):
-                console.log(gameWinner+="Player wins!")
+            case ((computerMove == "rock" && userMove == "paper") || 
+            (computerMove == "scissors" && userMove == "rock") ||
+            (computerMove == "paper" && userMove == "scissors")):
+                winnerEl.textContent= `${gameWinner} "Player wins!"`;
                 playerCounter++;
                 totalGames++;
                 break;
-                case ((userMove == "Rock" && computerMove == "Paper") || 
-                (userMove == "Scissors" && computerMove == "Rock") ||
-                (userMove == "Paper" && computerMove == "Scissors")):
-                console.log(gameWinner+="Computer wins!")
+                case ((userMove == "rock" && computerMove == "paper") || 
+                (userMove == "scissors" && computerMove == "rock") ||
+                (userMove == "paper" && computerMove == "scissors")):
+                winnerEl.textContent= `${gameWinner} "Computer wins!"`;
                 ComputerCounter++;
                 totalGames++;
                 break;
             default:
-                console.log("Try again with a valid option");
+                winnerEl.textContent= "Try again with a valid option";
                 break;
         }
     return ComputerCounter> playerCounter ? "Computer wins the whole match!" : "Player wins the whole match!"
 
     }
-
-console.log(playGame());
